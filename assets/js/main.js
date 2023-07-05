76ff4088198bf2667e1a1a9c9b454804c222518c
Uv0109 <utkarshvermalw@gmail.com>
2023-07-05 06:51:44 +0530

    big change

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,51 +133,51 @@
     })
   }
 
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
+    window.addEventListener('load', () => {
+      let portfolioContainer = select('.portfolio-container');
+      if (portfolioContainer) {
+        let portfolioIsotope = new Isotope(portfolioContainer, {
         itemSelector: '.portfolio-item'
-      });
+        });
 
       let portfolioFilters = select('#portfolio-flters li', true);
 
       on('click', '#portfolio-flters li', function(e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
+          portfolioFilters.forEach(function(el) {
+            el.classList.remove('filter-active');
+          });
         this.classList.add('filter-active');
 
-        portfolioIsotope.arrange({
+          portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
+          });
+          portfolioIsotope.on('arrangeComplete', function() {
           AOS.refresh()
-        });
-      }, true);
-    }
+          });
+        }, true);
+      }
 
   });
 
 
-  const portfolioLightbox = GLightbox({
+ const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
  
   new Swiper('.portfolio-details-slider', {
-    speed: 400,
+   speed: 400,
     loop: true,
     autoplay: {
       delay: 5000,
       disableOnInteraction: false
-    },
+      },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
-    }
+   }
   });
 
 
